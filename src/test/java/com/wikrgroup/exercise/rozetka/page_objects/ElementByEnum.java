798b9aef66e3b4edf640b9bc19291217bc50572b
shyster <kulasov@gmail.com>
2018-02-05 20:16:12 +0200

    refactored

--- src/test/java/com/wikrgroup/exercise/rozetka/page_objects/ElementByEnum.java
+++ src/test/java/com/wikrgroup/exercise/rozetka/page_objects/ElementByEnum.java
@@ -3,36 +3,28 @@ package com.wikrgroup.exercise.rozetka.page_objects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 /**
  * Created by Vladislav Kulasov on 04.02.2018.
  */
 public class ElementByEnum {
     public static WebElement getWebElement(WebDriver driver, PoEnum uiObjects) {
-        List<PoEnum> enums = new ArrayList<>();
+        Deque<PoEnum> enums = new LinkedList<>();
         WebElement element = null;
         PoEnum current = uiObjects;
         int i = 0;
-        while (true) {
-            enums.add(current);
+        do {
             i++;
             if (i >= 100) {
                 throw new RuntimeException("Looping or a lot of elements on page more than 100");
             }
-
-            if (null == current.getParentName()) {
-                break;
-            }
-
+            enums.push(current);
             current = (PoEnum) current.getParentName();
-        }
+        } while (!(null == current));
 
-        ListIterator elements = enums.listIterator(enums.size());
-        while (elements.hasPrevious()) {
-            PoEnum en = (PoEnum) elements.previous();
+        while (!enums.isEmpty()) {
+            PoEnum en = enums.pop();
             element = element == null ? driver.findElement(en.getBy()) : element.findElement(en.getBy());
         }
 
